8ff5e4384df74473199103a3bd0b1143a3f7897c
Alexey <akrysov@gmail.com>
2016-09-18 22:34:56 +0300

    Первое приложение на Angular2
    См.: https://angular.io/docs/ts/latest/quickstart.html
    Вначале провести (установка пакетов и typings):
    npm install
    npm run typings install
    И зпаустить приложением с использованием npm:
    npm start

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Алексей on 18.09.2016.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
