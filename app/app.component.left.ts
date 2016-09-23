2e67506c7fdfa2aa1ad27117385c2965ffadda94
Alexey <akrysov@gmail.com>
2016-09-24 00:25:21 +0300

    Некоторые изменения в файлах.

--- app/app.component.left.ts
+++ app/app.component.left.ts
@@ -4,7 +4,14 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-left',
-    template: require('./app.component.left.html'),
+    template:
+`
+    <div style="width: 20%;border: 1px solid black;">
+        left, menu111
+    </div>
+`
+  //  template: require('./app.component.left.html'),
+    //templateUrl: './app.component.left.html',
 })
 export class LeftComponent { }
 
