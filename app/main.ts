02bdc6895a1ad9cc707919fa95614d45dce85c69
Alexey <akrysov@gmail.com>
2016-09-24 08:44:39 +0300

    =Fix ProdMode, было предупреждение что Angular 2 работает в режиме разработки, нужно вызвать режим production.

--- app/main.ts
+++ app/main.ts
@@ -3,5 +3,11 @@
  */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
+import {enableProdMode} from '@angular/core';
+
+// Angular 2 is running in the development mode. Call enableProdMode()
+// https://github.com/angular/angular/issues/6189
+enableProdMode();
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
